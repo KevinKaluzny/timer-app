fd065755f052f5b0ad28a88dce9b4bf3a6b08656
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-08 12:12:48 +0200

    Customize component Timer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import Timer from './components/Timer/Timer.js';
-import Time from "./components/Time/Time";
-import Button from "./components/Button/Button";
 
 const App = () => {
   return (
-    <Timer>
-      <Time />
-      <Button>Start</Button>
-      <Button>Stop</Button>
-      <Button>Reset</Button>
-    </Timer>
+    <Timer />
   );
 };
 
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,18 @@
 import styles from './Timer.module.scss';
+import Time from "../Time/Time";
+import Button from "../Button/Button";
 
 const Timer = props => {
-    return (<div className={styles.timer}>{props.children}</div>);
+    return (
+        <div className={styles.timer}>
+            <Time />
+            <div>
+                <Button>Start</Button>
+                <Button>Stop</Button>
+                <Button>Reset</Button>
+            </div>
+        </div>
+    );
 }
 
 export default Timer;
