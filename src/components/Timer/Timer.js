6059eda3669bd960de7987d362e9fbb559cc9085
Kevin Kaluzny <kevin.evangelo.k@gmail.com>
2026-02-17 15:31:26 +0100

    Resolve useEffect dependency warning for interval cleanup
    Adjusted the cleanup effect in Timer.js to prevent the
    "react-hooks/exhaustive-deps" warning related to the `timer` dependency.
    
    The effect is intended to run only once (on mount/unmount) to clear
    the interval on component unmount. Added an ESLint disable comment
    to explicitly suppress the warning and document the intended behavior.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -31,6 +31,7 @@ const Timer = () => {
         return () => {
             if(timer) clearInterval(timer);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
